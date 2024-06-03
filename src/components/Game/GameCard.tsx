81d867f6bb99d2a8cb66f079040deeb6db5292f8
Christopher <hej@christopherwilliams.se>
2024-06-03 16:18:37 +0200

    Working on GameCard, and platform Icons

---
+++ src/components/Game/GameCard.tsx
@@ -0,0 +1,23 @@
+import { Game } from "../../hooks/useGames";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import PlatformIconsList from "../Platform/PlatformIconsList";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconsList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
+export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: {platform: Platform}[];
+  }
+  
+  interface FetchGamesResponse {
+    const: number;
+    results: Game[];
+  }
+
+
+const useGames = () => {
+
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+    
+    useEffect(() => {
+        apiClient
+        .get<FetchGamesResponse>("/games")
+        .then((res) => setGames(res.data.results))
+        .catch((err) => setError(err.message));
+    }, []);
+
+    return { games, error };
+
+}
+
+export default useGames;
