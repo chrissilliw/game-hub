e5b67830a60695b9652dcfaa9b6fa8a6a97f9c4e
Christopher <hej@christopherwilliams.se>
2024-06-06 12:17:23 +0200

    Updating baseURL.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: '/api',
+    baseURL: 'https://api.rawg.io/api',
     params: {
         key: import.meta.env.VITE_RAW_SECRET_KEY
     }
