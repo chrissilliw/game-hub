648a0060bc6d3ee9783647287a2b6f2ed74c885a
Christopher <hej@christopherwilliams.se>
2024-06-06 13:25:28 +0200

    Updating vite.config.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
+    // baseURL: '/api',
     baseURL: '/api',
     params: {
         key: import.meta.env.VITE_RAW_SECRET_KEY
--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://api.rawg.io/api',
+        target: 'https://api.rawg.io/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
 })
+
+// export default defineConfig({
+//   plugins: [react()]
+// })
