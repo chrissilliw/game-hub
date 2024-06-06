1b71c61155193526120a6a4f6585b4bdaff90832
Christopher <hej@christopherwilliams.se>
2024-06-06 13:27:56 +0200

    Updating api-client and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.rawg.io/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
-
 // export default defineConfig({
-//   plugins: [react()]
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://api.rawg.io/api',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
 // })
+
+export default defineConfig({
+  plugins: [react()]
+})
